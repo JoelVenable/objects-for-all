ecd2e9778763115d16b6cd2880f1141f22e65e2e
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-04-29 15:09:28 -0500

    Added junk adder that can also create new categories

---
+++ src/scripts/buttons.js
@@ -0,0 +1,72 @@
+buttons = [{
+    name: "Get Junk",
+    btnId: "#btn-getJunk",
+    function: () => {
+      API.getJunk()
+        .then(result => {
+          console.log("junk", result);
+          buildJunkDom(result);
+        });
+    }
+  },
+  {
+    name: "Get Categories",
+    btnId: "#btn-getCategories",
+    function: () => {
+      API.getCategories()
+        .then(result => {
+          console.log("category", result);
+          buildCategoryDom(result);
+        });
+    }
+  },
+  {
+    name: "Get Category Details",
+    btnId: "#btn-getCategoryDetails",
+    function: () => {
+      API.getCategoriesWithJunk()
+        .then(result => {
+          console.log("category list", result);
+          buildCategoryDetailDom(result);
+        });
+    }
+  },
+  {
+    name: "Add a Category",
+    btnId: "#btn-addCategory",
+    function: () => {
+      // build DOM elements
+      mainContainer.innerHTML = `
+        <h2>More Junk Categories</h2>
+        <fieldset>
+            <label for="junk__category">Name:</label>
+            <input id="junk__category" class="junk__category" type="text" autofocus />
+        </fieldset>
+        <button class="btn category__save" id="btn-saveCategory">Save Junk Category</button>`;
+
+      //  add Event Listener
+      document.querySelector("#btn-saveCategory").addEventListener("click", event => {
+        const category = document.querySelector(".junk__category").value;
+        // Once you have collected all the values, build your data structure
+        const categoryObj = {
+          name: category
+        };
+        API.save("category", categoryObj)
+          .then(parsedResult => {
+            console.log("what is the result", parsedResult);
+          });
+      })
+    }
+  },
+  {
+    name: "Add Junk",
+    btnId: "#btn-addJunk",
+    function: buildDOMJunkAdder
+  }
+]
+
+
+
+
+
+//  button functions
--- src/scripts/dom-junk.js
+++ src/scripts/dom-junk.js
@@ -19,3 +19,74 @@ const buildJunkDetailDom = (data) => {
         mainContainer.innerHTML += junkDetailDom(element);
     });
 };
+
+
+
+function buildDOMJunkAdder() {
+    API.getCategories()
+        .then(categoryList => {
+            let categorySelect = "";
+            categoryList.forEach(category => {
+                categorySelect += `<option value="${category.id}">${category.name}</option>`;
+            });
+            mainContainer.innerHTML = `
+          <h2>Add Junk</h2>
+          <fieldset>
+            <div class="form-row">
+              <label for="junk__name">Name:</label>
+              <input id="junk__name" class="junk__name" type="text" autofocus />
+            </div>
+            <div class="form-row">
+              <label for="junk__category">Category:</label>
+              <select id="junk__category">
+                ${categorySelect}
+              </select>
+            </div>
+            <div class="form-row">
+              <label for"new__category">New category?</label>
+              <input type="checkbox" id="add-category" />
+            </div>
+            <div class="form-row">
+                <label for="new__category-input">New Category Name</label>
+                <input type="text" id="new__category-input" />
+            </div>
+          </fieldset>
+          <button class="btn category__save" id="btn-saveJunk">Save Junk</button>`;
+        })
+        .then(addJunkEventHandler);
+}
+
+function addJunkEventHandler() {
+    document.querySelector("#btn-saveJunk").addEventListener("click", saveJunk);
+}
+
+function saveJunk() {
+    let newJunk = {
+        name: document.getElementById("junk__name").value,
+        categoryId: document.getElementById("junk__category").value
+    }
+    const newCategory = document.getElementById("add-category").checked; // Boolean
+    const newCategoryInput = {
+        name: document.getElementById("new__category-input").value
+    };
+    // Once you have collected all the values, build your data structure
+
+    function saveCategory(categoryObj) {
+        return API.save("category", categoryObj);
+    }
+
+    function saveJunk(junkObj) {
+        API.save("junk", junkObj);
+    }
+
+    if (newCategory) {
+        saveCategory(newCategoryInput)
+            .then(savedCategory => {
+                newJunk.categoryId = savedCategory.id;
+                saveJunk(newJunk);
+            })
+    } else {
+        saveJunk(newJunk);
+    }
+
+}
